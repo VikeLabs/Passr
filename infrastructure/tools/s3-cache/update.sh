f61bd061f7d5848ae97e75e4942d1683ab77e4ec
braidencutforth <braiden89@gmail.com>
2020-10-12 15:09:06 -0600

    Remove logging from update script

--- infrastructure/tools/s3-cache/update.sh
+++ infrastructure/tools/s3-cache/update.sh
@@ -7,14 +7,9 @@ FUNCTION_NAME="passr-s3-cache-lambda"
 cd infrastructure/tools/s3-cache
 npx rollup -c rollup.config.js
 cd build/
-ls
 zip "../$ZIP_FILE" *
 cd ..
 
-ls
-
-echo $ZIP_FILE
-echo $S3_BUCKET
 aws s3 cp "$ZIP_FILE" "s3://$S3_BUCKET/$ZIP_FILE"
 
 aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket "$S3_BUCKET" --s3-key "$ZIP_FILE" --publish

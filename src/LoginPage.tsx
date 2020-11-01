5393a5a9b0fced538b22b7eb01a50658007e8783
snorrie <Samantha.Norrie@flir.com>
2020-10-31 18:50:10 -0700

    PAS-41 created container for login page (WIP)

---
+++ src/LoginPage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './LoginPage.css';
+
+function LoginPage() {
+	return (
+		<div className="Login-page-container">
+			<img src={logo} className="Passr-logo" alt="logo" />
+			<form>
+				<label className="Email-label">Email*</label>
+				<input type="text" id="Email" className="Email"/>
+				<label className="Password-label">Password*</label>
+  			<input type="text" className="Password"/>
+			</form>
+			<button type="button" className="Login-button">Login</button>
+      <a
+        className="Password-link"
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Forgot your password?
+      </a>
+			<a
+        className="New-user-link"
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        New user? Create a Passr account
+      </a>
+		</div>
+	);
+}
+
+export default LoginPage;

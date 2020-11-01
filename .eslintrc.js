933b35259b2d83da7b8a24a20c6a2e9958e5af31
polygonart6777 <buricd6777@gmail.com>
2020-10-31 21:19:45 -0700

    PAS-46 Adds prettier and eslint to Passr (#19)
    * PAS-46 (WIP) Adds prettier and eslint to Passr
    -Adds eslint and prettier to the configuration file
    -Adds npm commands to lint the project, one with fixing one with not
    -Spcifies configuration for VSCode to auto lint but not to auto save
    lint changes and includes recommended extensions.
    -Adds pre-commit hook to re-format files that are marked as "staged"
    before you commit.
    
    * PAS-46 WIP deletes duplicate line and added simpler formatting to regex
    
    * PAS-46 Adds documentation and a test for lint-staging
    
    * PAS-46 testing git hook with eslint
    
    * PAS-46 (WIP) Adds better lint configuration file.  Documentation stil needs to be finished with justification for each configuration tool.
    
    * PAS-46 (WIP) changes name for eslint to lint
    
    * PAS-46 updated react-scripts and dependencies in order to help build run sucessfully
    
    * PAS-46 updated react-scripts and dependencies in order to help build run sucessfully
    
    * PAS-46 finished documentation
    
    * PAS-46 removes eslint git commit check and adjusts the documentation
    
    * PAS-46 Addresses code review comments
    -Removes unnecessary files from gitignore
    -Addresses typos in documentation
    
    * PAS-46 Addresses code review comments
    -Installs testing library plugin so that we can lint test files
    -Addresses eslint errors by moving functions before they are used
    -Updates plugins to the most current version
    
    * PAS-46 Fixes typos in documentation and removes unnecessary files from gitignore.
    
    * PAS-46 Adds new npm scripts to the ReadMe file
    
    * PAS-46- Removes double word from ReadMe. Why must I spot typos right after I push up changes?
    
    * PAS-46 Adds path fix for Windows

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	env: {
+		browser: true,
+		node: true,
+		es6: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'react/prop-types': 'off',
+		'react/no-unused-prop-types': 'warn',
+		'prettier/prettier': 'warn',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};

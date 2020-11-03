7ce1f45a6e44de0dad52630c092c139c075a6319
Seiya Terada <seiyaterada@uvic.ca>
2020-11-02 20:04:03 -0800

    Update CSS for main action button

--- src/components/MainActionButton.tsx
+++ src/components/MainActionButton.tsx
@@ -11,8 +11,9 @@ const MainButton = styled.button`
 	background-color: #4961e1;
 	color: white;
 	font-family: Montserrat, Arial;
-	font-size: 0.5em;
-	padding: 1 rem 3rem;
+	font-size: 1em;
+	border-radius: 6px;
+	padding: 0.5rem 2rem;
 `;
 
 function MainActionButton({ text, onClick }: mainActionButtonInterface) {

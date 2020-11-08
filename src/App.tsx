352dc668a615c49d1fccb269d432dfd1c3e6a5df
dereklowlind <38629827+dereklowlind@users.noreply.github.com>
2020-11-08 12:44:52 -0800

    first commit  PAS-48 Create a logo component (#21)
    * first commit  PAS-48 Create a logo component
    
    * add alt prop to log img tag to fix build error
    
    * fix warning about logo variable being unused
    
    * deleted commented out code to clean up App.tsx
    
    * add newline to end of Logo.tsx
    
    * Rebase Master and Lint Files
    
    Co-authored-by: braidencutforth <braiden89@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Logo from './molecules/Logo';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+				{/* <img src={logo} className="App-logo" alt="logo" /> */}
+				<Logo width="300px" height="300px" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
---
+++ src/molecules/Logo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+interface LogoProps {
+	height: string;
+	width: string;
+}
+
+function Logo({ width, height }: LogoProps) {
+	return (
+		<img src={logo} style={{ width: width, height: height }} alt="logo" />
+	);
+}
+
+export default Logo;

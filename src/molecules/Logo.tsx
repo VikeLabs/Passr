97fd64eff5cf46e731e5fcd5852124286b8b2055
braidencutforth <braiden89@gmail.com>
2020-11-08 13:42:01 -0700

    Rebase Master and Lint Files

--- src/molecules/Logo.tsx
+++ src/molecules/Logo.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from '../logo.svg';
 
 interface LogoProps {
-    height: string
-    width: string
-  }
-
-function Logo({width, height}:LogoProps) {
-    return (
-        <img src={logo} style={{width: width, height: height}} alt="logo"/>
-    )
+	height: string;
+	width: string;
 }
-    
-export default Logo
+
+function Logo({ width, height }: LogoProps) {
+	return (
+		<img src={logo} style={{ width: width, height: height }} alt="logo" />
+	);
+}
+
+export default Logo;

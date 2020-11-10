838cc244b13f8af24c2dbdb872e571bc50fb2926
seiyaterada <54200250+seiyaterada@users.noreply.github.com>
2020-11-10 14:59:07 -0800

    Pas 43 create main action button (#25)
    * PAS-43 Create Main Action Button
    
    * Update CSS for main action button
    
    * Enable eslint and remove font family in styled components
    
    * Changed prop from text string to React.ReactNode and added more styling
    
    * Added missing bracket
    
    Co-authored-by: Seiya Terada <seiyaterada@uvic.ca>

---
+++ src/components/MainActionButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export interface mainActionButtonInterface {
+	children: React.ReactNode;
+	onClick: () => void;
+}
+
+const MainActionButton = styled.button`
+	background-color: #4961e1;
+	color: white;
+	font-size: 1em;
+	border-radius: 6px;
+	padding: 0.5rem 2rem;
+	width: 100%;
+	border: none;
+	&:hover {
+		background-color: #354bc4;
+	}
+`;
+
+function MainButton({ children, onClick }: mainActionButtonInterface) {
+	return <MainActionButton onClick={onClick}>{children}</MainActionButton>;
+}
+
+export default MainButton;

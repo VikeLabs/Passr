24f70e6fabb98947ebaa519bb54ccca815510de7
snorrie <Samantha.Norrie@flir.com>
2020-11-16 22:21:09 -0800

    PAS-60 header componenet

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export interface TopBarInterface {
+	text: string;
+}
+
+const TopBarStyle = styled.div`
+	display: flex;
+	flex-direction: column;
+	font-weight: 600;
+	height: 3em;
+	font-size: 1em;
+	letter-spacing: 0.2px;
+	text-align: center;
+	background: #ffffff;
+	color: #002366;
+`;
+
+function TopBar({ text }: TopBarInterface) {
+	return (
+		<TopBarStyle>
+			<p>Passr</p>
+		</TopBarStyle>
+	);
+}
+
+export default TopBar;

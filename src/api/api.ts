6d4a66cc863a070658b463f5ccba20714c1e0434
Braiden Cutforth <braiden89@gmail.com>
2020-12-17 19:22:22 -0700

    Initial Mock API (#27)
    * Initial Mock API
    
    * PAS-15: Add fraction support
    
    - Add fractions to api and mock data
    - Re-export api functions in index
    
    * Review fixes
    
    * Add due date as string

---
+++ src/api/api.ts
@@ -0,0 +1,5 @@
+import { Fall2020 } from './mock';
+
+export function getCurrentSemester() {
+	return Promise.resolve(Fall2020);
+}
---
+++ src/api/index.ts
@@ -0,0 +1,24 @@
+export { getCurrentSemester } from './api';
+
+export interface Fraction {
+	numerator: number;
+	denominator: number;
+}
+
+export interface CourseItem {
+	name: string;
+	weight: number;
+	grade?: number | Fraction;
+	dueDate?: Date;
+}
+
+export interface Course {
+	name: string;
+	crn?: number;
+	items: CourseItem[];
+}
+
+export interface Semester {
+	name: string;
+	courses: Course[];
+}

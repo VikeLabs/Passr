7f3ff95ea23e8d9fbaa0ab9f0c6367a3d690079b
isaiahdoyle <isaiahdoyle56@gmail.com>
2020-12-28 18:28:46 -0800

    Add delete button

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import MainButton from './MainActionButton';
+
+const MainDeleteButton = styled(MainButton)`
+	background-color: #b80f0a;
+	color: #ffffff;
+	&:hover {
+		background-color: #b80f0a;
+	}
+`;
+
+export default MainDeleteButton;

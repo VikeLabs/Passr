59db614a7058a8a8de0b9372eabeb72ade3732ca
isaiahdoyle <isaiahdoyle56@gmail.com>
2021-01-04 19:30:34 -0800

    fix variants

--- src/components/MainActionButton.tsx
+++ src/components/MainActionButton.tsx
@@ -3,41 +3,14 @@ import styled from 'styled-components';
 import { variant } from 'styled-system';
 import { inherits } from 'util';
 
+type Variant = 'success' | 'delete';
 export interface mainActionButtonInterface {
 	children: React.ReactNode;
 	onClick: () => void;
+	variant?: Variant;
 }
 
-// const MainActionButton = styled.button`
-// 	// background-color: #4961e1;
-// 	color: white;
-// 	font-size: 1em;
-// 	border-radius: 6px;
-// 	padding: 0.5rem 2rem;
-// 	width: 100%;
-// 	border: none;
-// 	&:hover {
-// 		background-color: #354bc4;
-// 	},
-// 	variant({
-// 		variants: {
-// 			success: {
-// 				background-color: #4961e1;
-// 				&:hover {
-// 					background-color: #354bc4;
-// 				}
-// 			}
-// 			delete: {
-// 				background-color: #b80f0a;
-// 				&:hover {
-// 					background-color: #850c04;
-// 				}
-// 			}
-// 		}
-// 	})
-// `;
-
-const MainActionButton = styled('button')(
+const MainActionButton = styled('button')<{ variant: Variant }>(
 	{
 		color: 'white',
 		fontSize: '1em',
@@ -67,10 +40,15 @@ const MainActionButton = styled('button')(
 function MainButton({
 	children,
 	onClick,
+	variant,
 	...props
 }: mainActionButtonInterface) {
 	return (
-		<MainActionButton variant="success" onClick={onClick} {...props}>
+		<MainActionButton
+			variant={variant || 'success'}
+			onClick={onClick}
+			{...props}
+		>
 			{children}
 		</MainActionButton>
 	);

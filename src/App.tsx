3bd8868ca244913fff123856f4bdcb88249e79f8
isaiahdoyle <isaiahdoyle56@gmail.com>
2021-01-04 22:41:33 -0800

    revert app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
 import React from 'react';
 import './App.css';
 import Logo from './molecules/Logo';
-import MainButton from './components/MainActionButton';
-import AddItemModal, { AddItemData } from './components/AddItemModal';
-import DeleteItemModal from './components/DeleteItemModal';
-
-// function del() {
-// 	console.log('assignment deleted');
-// }
-
-// function add({ name, date, weight, grade }: AddItemData) {
-// 	console.log(name);
-// 	console.log(date);
-// 	console.log(weight);
-// 	console.log(grade);
-// }
-
-// function close() {
-// 	console.log('modal closed');
-// }
 
 function App() {
 	return (
@@ -26,20 +8,9 @@ function App() {
 			<header className="App-header">
 				{/* <img src={logo} className="App-logo" alt="logo" /> */}
 				<Logo width="300px" height="300px" />
-				{/* <DeleteItemModal
-					handleDelete={() => del()}
-					handleClose={() => close()}
-				></DeleteItemModal> */}
-				{/* <AddItemModal
-					handleSubmit={(data) => add(data)}
-					handleClose={() => close()}
-				></AddItemModal> */}
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
-				{/* <MainButton variant="primary" onClick={() => del()}>
-					Delete
-				</MainButton> */}
 				<a
 					className="App-link"
 					href="https://reactjs.org"

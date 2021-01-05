a98be546ca422e62a7680cdf3f4406dba91847d1
isaiahdoyle <isaiahdoyle56@gmail.com>
2021-01-04 18:40:07 -0800

    variant changes

--- src/components/MainActionButton.tsx
+++ src/components/MainActionButton.tsx
@@ -1,23 +1,68 @@
 import React from 'react';
 import styled from 'styled-components';
+import { variant } from 'styled-system';
+import { inherits } from 'util';
 
 export interface mainActionButtonInterface {
 	children: React.ReactNode;
 	onClick: () => void;
 }
 
-const MainActionButton = styled.button`
-	background-color: #4961e1;
-	color: white;
-	font-size: 1em;
-	border-radius: 6px;
-	padding: 0.5rem 2rem;
-	width: 100%;
-	border: none;
-	&:hover {
-		background-color: #354bc4;
-	}
-`;
+// const MainActionButton = styled.button`
+// 	// background-color: #4961e1;
+// 	color: white;
+// 	font-size: 1em;
+// 	border-radius: 6px;
+// 	padding: 0.5rem 2rem;
+// 	width: 100%;
+// 	border: none;
+// 	&:hover {
+// 		background-color: #354bc4;
+// 	},
+// 	variant({
+// 		variants: {
+// 			success: {
+// 				background-color: #4961e1;
+// 				&:hover {
+// 					background-color: #354bc4;
+// 				}
+// 			}
+// 			delete: {
+// 				background-color: #b80f0a;
+// 				&:hover {
+// 					background-color: #850c04;
+// 				}
+// 			}
+// 		}
+// 	})
+// `;
+
+const MainActionButton = styled('button')(
+	{
+		color: 'white',
+		fontSize: '1em',
+		borderRadius: '6px',
+		padding: '0.5rem 2rem',
+		width: '100%',
+		border: 'none',
+	},
+	variant({
+		variants: {
+			success: {
+				backgroundColor: '#4961e1',
+				'&:hover': {
+					backgroundColor: '#354bc4',
+				},
+			},
+			delete: {
+				backgroundColor: '#b80f0a',
+				'&:hover': {
+					backgroundColor: '#850c04',
+				},
+			},
+		},
+	})
+);
 
 function MainButton({
 	children,
@@ -25,7 +70,7 @@ function MainButton({
 	...props
 }: mainActionButtonInterface) {
 	return (
-		<MainActionButton onClick={onClick} {...props}>
+		<MainActionButton variant="success" onClick={onClick} {...props}>
 			{children}
 		</MainActionButton>
 	);

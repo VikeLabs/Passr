1017a8cff5eff46832539188901b0e41a1fd07d1
isaiahdoyle <71530236+isaiahdoyle@users.noreply.github.com>
2021-01-07 18:15:39 -0800

    Delete button (#39)
    * Add delete button
    
    * Modify MainActionButton for styling (...props)
    
    * Change DeleteButton name for exporting
    
    * variant changes
    
    * fix variants
    
    * add more variants
    
    * install styled-system

--- src/components/MainActionButton.tsx
+++ src/components/MainActionButton.tsx
@@ -1,26 +1,84 @@
 import React from 'react';
 import styled from 'styled-components';
+import { variant } from 'styled-system';
 
+export type Variant =
+	| 'primary'
+	| 'secondary'
+	| 'positive'
+	| 'negative'
+	| 'alternate';
 export interface mainActionButtonInterface {
 	children: React.ReactNode;
 	onClick: () => void;
+	variant?: Variant;
 }
 
-const MainActionButton = styled.button`
-	background-color: #4961e1;
-	color: white;
-	font-size: 1em;
-	border-radius: 6px;
-	padding: 0.5rem 2rem;
-	width: 100%;
-	border: none;
-	&:hover {
-		background-color: #354bc4;
-	}
-`;
+const MainActionButton = styled('button')<{ variant: Variant }>(
+	{
+		color: 'white',
+		fontSize: '1em',
+		borderRadius: '6px',
+		padding: '0.5rem 2rem',
+		width: '100%',
+		border: 'none',
+	},
+	variant({
+		variants: {
+			primary: {
+				backgroundColor: '#4961e1',
+				color: '#FFFFFF',
+				'&:hover': {
+					backgroundColor: '#354bc4',
+				},
+			},
+			secondary: {
+				backgroundColor: '#FFFFFF',
+				color: '#002366',
+				'&:hover': {
+					backgroundColor: '#E5F2FC',
+				},
+			},
+			positive: {
+				backgroundColor: '#35BEA0',
+				color: '#FFFFFF',
+				'&:hover': {
+					backgroundColor: '#2A967E',
+				},
+			},
+			negative: {
+				backgroundColor: '#b80f0a',
+				color: '#FFFFFF',
+				'&:hover': {
+					backgroundColor: '#880B07',
+				},
+			},
+			alternate: {
+				backgroundColor: '#e0e0e0',
+				color: '#002366',
+				'&:hover': {
+					backgroundColor: '#d0d0d0',
+				},
+			},
+		},
+	})
+);
 
-function MainButton({ children, onClick }: mainActionButtonInterface) {
-	return <MainActionButton onClick={onClick}>{children}</MainActionButton>;
+function MainButton({
+	children,
+	onClick,
+	variant,
+	...props
+}: mainActionButtonInterface) {
+	return (
+		<MainActionButton
+			variant={variant || 'primary'}
+			onClick={onClick}
+			{...props}
+		>
+			{children}
+		</MainActionButton>
+	);
 }
 
 export default MainButton;

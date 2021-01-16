83e8480aee5c3ec2c80aaf6c5d39294de2ec3c43
Seiya Terada <seiyaterada@uvic.ca>
2021-01-16 14:46:01 -0800

    PAS-37 Create Grade Calculator

---
+++ src/components/GradeCalculator.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Course, Semester } from '../api';
+import { getCurrentSemester } from '../api/api';
+
+const semester = getCurrentSemester();
+
+function currentGradeCalculator(course: Course) {
+	course.items.map((item, index) => {
+		return item.grade;
+	});
+}
+
+// function earnedGradeCalculator{}
+
+// function lostGradeCalculator{}
+
+// function maxGradeCalculator{}
+
+// function averageGradeNeededCalculator{}
+export default currentGradeCalculator;

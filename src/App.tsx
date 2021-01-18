25078d7be2e2582b79c293d3c5c1c35e1d020e65
snorrie <Samantha.Norrie@flir.com>
2021-01-17 17:34:38 -0800

    [PAS-58] informal revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Course, getCurrentSemester } from './api';
-import './App.css';
-import GradeBookContentContainer from './components/GradeBookContentContainer';
-import Logo from './molecules/Logo';
+import React from 'react';
+import Pages from './pages';
 
 function App() {
-	const [course, setCourse] = useState<Course | null>(null);
-	useEffect(() => {
-		(async () => {
-			const sem = await getCurrentSemester();
-			setCourse(sem.courses[0]);
-		})();
-	}, []);
-
-	useEffect(() => {
-		// Update semester with API
-	}, [course]);
-
-	function updateCourse(course: Course) {
-		setCourse(course);
-	}
-	return (
-		<div className="App">
-			{course != null || <h1>Loading</h1>}
-			{course && (
-				<GradeBookContentContainer
-					course={course}
-					updateCourse={updateCourse}
-				/>
-			)}
-		</div>
-	);
+	return <Pages />;
 }
 
 export default App;

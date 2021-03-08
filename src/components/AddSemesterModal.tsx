c26e6ed959681c852a8516470350e9a32c9fff82
Administrator <admin@192.168.50.74>
2021-03-08 12:48:11 -0800

    Created AddSemesterModal

---
+++ src/components/AddSemesterModal.tsx
@@ -0,0 +1,84 @@
+import React, { useContext, useState } from 'react';
+import styled, { ThemeContext } from 'styled-components';
+import DefaultModal from './DefaultModal';
+import TextInput from './TextInput';
+import Logo from '../molecules/Logo';
+
+const Body = styled.div`
+	height: 100%;
+	width: 100%;
+	display: grid;
+	grid-template-rows: 1fr 4fr 2fr;
+	grid-template-areas:
+		'desc'
+		'logo'
+		'input';
+	justify-items: center;
+`;
+const Desc = styled.p`
+	grid-area: desc;
+	font-size: ${({ theme }) => theme.fontSizes.xs};
+	margin: 1em 0 0 0.2em;
+`;
+
+const BodyLogo = styled(Logo)`
+	grid-area: logo;
+`;
+
+const InputField = styled.div`
+	grid-area: input;
+	padding: 0 0 0.5em 0;
+	margin: 0 2.2em 0 2em;
+	justify-content: center;
+	align-items: center;
+`;
+
+export interface AddSemesterInterface {
+	handleSubmit: (data: AddSemesterData) => void;
+	handleClose: () => void;
+}
+
+export interface AddSemesterData {
+	name: string;
+}
+
+function AddSemesterModal({ handleSubmit, handleClose }: AddSemesterInterface) {
+	const [name, setName] = useState('');
+
+	const theme = useContext(ThemeContext);
+
+	function onSubmit({ name }: AddSemesterData) {
+		handleSubmit({ name });
+		handleClose();
+	}
+
+	return (
+		<DefaultModal
+			headerColor={theme.colors.primary[0]}
+			handleClose={handleClose}
+			header="Add Semester"
+			primaryButton="Submit"
+			primaryVariant="primary"
+			secondaryButton="Cancel"
+			secondaryVariant="alternate"
+			handlePrimary={() => onSubmit({ name })}
+		>
+			<Body>
+				<Desc>Enter a name for the new semester.</Desc>
+				<BodyLogo width="13em" height="100%" />
+				<InputField>
+					<TextInput
+						label="Name"
+						value={name}
+						onChange={(e) => {
+							setName(e.currentTarget.value);
+						}}
+						required={true}
+					></TextInput>
+				</InputField>
+			</Body>
+		</DefaultModal>
+	);
+}
+
+export default AddSemesterModal;

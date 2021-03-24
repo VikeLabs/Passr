e822ee2321f1b6ebe2fb75c0e555ced3f9da2640
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2021-03-24 12:22:32 -0700

    [PAS-68] Gradebook Accordion Header +Utils file (#60)
    * [PAS-68] utils, gradebook header, and new component for gradebook header
    
    * [PAS-68] Formatting for gradebook header
    
    * Update src/Utils.tsx with specified return
    
    Co-authored-by: Braiden Cutforth <braiden89@gmail.com>
    
    * [PAS-68] Utils function param change + reformatting of gradebook header
    
    * [PAS-68] removed comment
    
    Co-authored-by: snorrie <Samantha.Norrie@flir.com>
    Co-authored-by: Braiden Cutforth <braiden89@gmail.com>

---
+++ src/components/HeaderGradeDisplay.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export interface Props {
+	label: string;
+	grade: string;
+}
+const HeaderDisplayContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+const Label = styled.p`
+	font-size: ${({ theme }) => theme.fontSizes.xs};
+	color: ${({ theme }) => theme.colors.primary[0]};
+	width: 17em;
+	margin: 0;
+	margin-bottom: 0.5em;
+`;
+const HeaderGradeDisplayContainer = styled.div`
+	background-color: ${({ theme }) => theme.colors.main[0]};
+	width: 15em;
+`;
+const GradeDisplayText = styled.h2`
+	color: ${({ theme }) => theme.colors.gray[3]};
+	padding-left: 1em;
+`;
+function OpenInfo() {
+	console.log('opened!');
+}
+function HeaderGradeDisplay({ label, grade, ...props }: Props) {
+	return (
+		<HeaderDisplayContainer {...props}>
+			<Label>{label}</Label>
+			<HeaderGradeDisplayContainer onClick={OpenInfo}>
+				{(grade === '' && <GradeDisplayText>N/A</GradeDisplayText>) || (
+					<GradeDisplayText>{grade}%</GradeDisplayText>
+				)}
+			</HeaderGradeDisplayContainer>
+		</HeaderDisplayContainer>
+	);
+}
+
+export default HeaderGradeDisplay;

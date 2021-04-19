7d1e65d8d54591b9fce062d62f7e65121966a135
lbare <levi.bare@gmail.com>
2021-04-19 14:17:09 -0700

    updated interfaces

--- src/api/mock.ts
+++ src/api/mock.ts
@@ -1,160 +1,197 @@
-import { Semester, Course, CourseItem } from './index';
+import {
+	SemesterInterface,
+	CourseInterface,
+	CourseItemInterface,
+} from './index';
 
-const Math100Items: CourseItem[] = [
+const Math100Items: CourseItemInterface[] = [
 	{
+		id: 'c234t2m7',
 		name: 'Assignment 1',
 		weight: 10,
 		grade: { numerator: 9, denominator: 10 },
 	},
 	{
+		id: '3mu824mp',
 		name: 'Assignment 2',
 		weight: 5,
 		grade: { numerator: 8, denominator: 10 },
 		dueDate: new Date('January 20, 2021'),
 	},
 	{
+		id: 'n8uc9pm0',
 		name: 'Assignment 3',
 		weight: 5,
-
 		dueDate: new Date('February 16, 2021'),
 	},
 	{
+		id: 'm893lwef',
 		name: 'Midterm 1',
 		weight: 20,
 	},
 	{
+		id: 'k329f7d9',
 		name: 'Midterm 2',
 		weight: 20,
 	},
 	{
+		id: 'io42fj4n',
 		name: 'Final',
 		weight: 40,
 	},
 ];
 
-const MATH100: Course = {
+const MATH100: CourseInterface = {
+	id: '84jvkq11',
 	name: 'Math 100',
-	items: Math100Items,
+	courseItems: Math100Items,
+	desiredGrade: 80,
 };
 
-const CSC110Items: CourseItem[] = [
+const CSC110Items: CourseItemInterface[] = [
 	{
+		id: 'vy8amquo',
 		name: 'Assignment 1',
 		weight: 5,
 		grade: { numerator: 9, denominator: 10 },
 	},
 	{
+		id: 'jb5wy7jx',
 		name: 'Assignment 2',
 		weight: 5,
 		grade: 75,
 	},
 	{
+		id: 'bmvnno73',
 		name: 'Assignment 3',
 		weight: 5,
 		grade: 80,
 	},
 	{
+		id: 'bnyjauol',
 		name: 'Assignment 4',
 		weight: 5,
 		grade: 85,
 	},
 	{
+		id: 'g0p3p59m',
 		name: 'Assignment 5',
 		weight: 5,
 		grade: { numerator: 3, denominator: 4 },
 	},
 	{
+		id: 'oxo34929',
 		name: 'Assignment 6',
 		weight: 5,
 	},
 	{
+		id: 'ybccq9iq',
 		name: 'Assignment 7',
 		weight: 5,
 	},
 	{
+		id: '8j6dxeye',
 		name: 'Assignment 8',
 		weight: 5,
 	},
 	{
+		id: 'xbovaqnv',
 		name: 'Midterm 1',
 		weight: 20,
 		grade: 60,
 	},
 	{
+		id: 'v40rpo7y',
 		name: 'Midterm 2',
 		weight: 20,
 		grade: 85,
 	},
 	{
+		id: 'j1no3ws6',
 		name: 'Midterm 3',
 		weight: 20,
 	},
 ];
 
-const CSC110: Course = {
+const CSC110: CourseInterface = {
+	id: 'ynlq4qre',
 	name: 'CSC 110',
-	items: CSC110Items,
+	courseItems: CSC110Items,
+	desiredGrade: 75,
 };
 
-const PHYS111Items: CourseItem[] = [
+const PHYS111Items: CourseItemInterface[] = [
 	{
+		id: '33yeqkq1',
 		name: 'Assignment 1',
 		weight: 5,
 		grade: 75,
 	},
 	{
+		id: 'znb1ueec',
 		name: 'Assignment 2',
 		weight: 5,
 		grade: 80,
 	},
 	{
+		id: 'b75dzn83',
 		name: 'Assignment 3',
 		weight: 5,
 	},
 	{
+		id: 'zliql2yv',
 		name: 'Assignment 4',
 		weight: 5,
 	},
 	{
+		id: 'w873iby6',
 		name: 'Lab 1',
 		weight: 4,
 		grade: 90,
 	},
 	{
+		id: 'acmxcbgp',
 		name: 'Lab 2',
 		weight: 4,
 		grade: { numerator: 7, denominator: 9 },
 	},
 	{
+		id: 'jt0a7l6y',
 		name: 'Lab 3',
 		weight: 4,
 	},
 	{
+		id: '4vendf7l',
 		name: 'Lab 4',
 		weight: 4,
 	},
 	{
+		id: 'd1vvm5ka',
 		name: 'Lab 5',
 		weight: 4,
 	},
 	{
+		id: 'fqemoguv',
 		name: 'Midterm',
 		weight: 20,
 		grade: { numerator: 13, denominator: 20 },
 	},
 	{
+		id: 'zzqyex7k',
 		name: 'Final',
 		weight: 40,
 	},
 ];
 
-const PHYS111: Course = {
+const PHYS111: CourseInterface = {
+	id: 'otdl0koo',
 	name: 'PHYS 111',
-	items: PHYS111Items,
+	courseItems: PHYS111Items,
 	desiredGrade: 60,
 };
 
-export const Fall2020: Semester = {
+export const Fall2020: SemesterInterface = {
+	id: '3oeg11pf',
 	name: 'Fall 2020',
 	courses: [MATH100, CSC110, PHYS111],
 };
--- src/components/GradeBookContentContainer.tsx
+++ src/components/GradeBookContentContainer.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import ActionButton from './ActionButton';
-import { Course, CourseItem } from '../api';
+import { CourseInterface, CourseItemInterface } from '../api';
 import GradeItemAccordion from './GradeItemAccordion';
 import AddItemModal, { AddItemData } from './AddItemModal';
 
@@ -65,8 +65,8 @@ const CourseItemTitle = styled.div`
 `;
 
 interface Props {
-	course: Course;
-	updateCourse: (course: Course) => void;
+	course: CourseInterface;
+	updateCourse: (course: CourseInterface) => void;
 }
 
 function GradeBookContentContainer({ course, updateCourse }: Props) {
@@ -84,10 +84,10 @@ function GradeBookContentContainer({ course, updateCourse }: Props) {
 	function openModal() {
 		setModalOpen(true);
 	}
-	function updateCourseItem(item: CourseItem, index: number) {
-		const newCourseItems = [...course.items];
+	function updateCourseItem(item: CourseItemInterface, index: number) {
+		const newCourseItems = [...course.courseItems];
 		newCourseItems[index] = item;
-		updateCourse({ ...course, items: newCourseItems });
+		updateCourse({ ...course, courseItems: newCourseItems });
 	}
 
 	return (
@@ -116,7 +116,7 @@ function GradeBookContentContainer({ course, updateCourse }: Props) {
 			</CourseItemTitle>
 
 			<CourseItemRow>
-				{course.items.map((item, index) => {
+				{course.courseItems.map((item, index) => {
 					console.log({ item, index });
 					return (
 						<GradeItemAccordion

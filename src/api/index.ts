98ac5c43b1a5e10065b7eb55e46c97dbbc345701
lbare <levi.bare@gmail.com>
2021-04-25 20:00:08 -0700

    added owner, createdAt, and updatedAt fields

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 export { getCurrentSemester } from './api';
 export type Grade = number | Fraction | undefined;
 
+/* These interfaces are models in our database */
 export interface Fraction {
 	numerator: number;
 	denominator: number;
@@ -8,6 +9,9 @@ export interface Fraction {
 
 export interface CourseItem {
 	id: string;
+	owner: string;
+	createdAt: Date;
+	updatedAt: Date;
 	name: string;
 	weight: number;
 	grade?: number | Fraction;
@@ -16,19 +20,26 @@ export interface CourseItem {
 
 export interface Course {
 	id: string;
+	owner: string;
+	createdAt: Date;
+	updatedAt: Date;
 	name: string;
-	crn?: number;
 	courseItems: CourseItem[];
 	desiredGrade: number;
 }
 
 export interface Semester {
 	id: string;
+	owner: string;
+	createdAt: Date;
+	updatedAt: Date;
 	name: string;
 	courses: Course[];
 }
 
 export interface User extends Document {
 	id: string;
+	createdAt: Date;
+	updatedAt: Date;
 	semesters: Semester[];
 }

03a1c9e529433aa3a9abef1003300bbce3beddc6
lbare <levi.bare@gmail.com>
2021-05-06 15:27:42 -0700

    Added Course functions

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,26 @@
 import { Fall2020 } from './mock';
+import { Course } from './index';
 
 export function getCurrentSemester() {
 	return Promise.resolve(Fall2020);
+}
+
+export async function createCourse(course: Course) {
+	course;
+	return false;
+}
+
+export async function updateCourse(course: Partial<Course>) {
+	course;
+	return false;
+}
+
+export async function getCourse(id: string) {
+	id;
+	return false;
+}
+
+export async function deleteCourse(id: string) {
+	id;
+	return false;
 }

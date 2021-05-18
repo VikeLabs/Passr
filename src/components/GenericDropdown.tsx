1b38752c6c6fcb5872ebf812f57c06e794ab58cc
Braiden Cutforth <braiden89@gmail.com>
2021-05-17 21:52:32 -0600

    [PAS-109] Update Profile Dropdown Sign in/Sign out behaviour (#75)
    * wip
    
    * Remove unused vars, improve sign out behaviour
    
    * Update useComponentVisible hook
    
    * Change the structure of the Generic Dropdown (Inheritance -> Composition)
    
    * PAS-109: Clean up Generic Dropdown
    
    - Extract function for setting dropdown list position
    - Extract variable for detecting outside click (useComponentVisible
    hook)
    - Remove console log from useSignedIn hook
    
    * PAS-109: Refine setting dropdown list location
    
    * PAS-109: Change DropdownButton to MenuButton
    
    - Rename DropdownButton to MenuButton for clarity
    
    * PAS-109: Remove unused import

--- src/components/GenericDropdown.tsx
+++ src/components/GenericDropdown.tsx
@@ -1,11 +1,10 @@
-import React, { useRef } from 'react';
-import DropdownList, { DropdownItem } from '../components/DropdownList';
+import React, { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import useComponentVisible from '../hooks/useComponentVisible';
+import { Link } from 'react-router-dom';
 
 interface Props {
 	buttonDisplay: React.ReactNode;
-	dropdownItems: DropdownItem[];
 }
 
 const DropdownButton = styled.button`
@@ -15,6 +14,26 @@ const DropdownButton = styled.button`
 	border: 1px, solid, black;
 `;
 
+const DropdownMenu = styled.ul<{ componentVisible: boolean }>`
+	list-style: none;
+	background-color: ${({ theme }) => theme.colors.main[0]};
+	text-align: start;
+	z-index: 2;
+	position: absolute;
+	border: 1px solid rgba(0, 0, 0, 0.04);
+	box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14);
+	padding: 0;
+	margin: 0;
+	border-radius: 10px;
+	border-radius: 0 0 10px 10px;
+	visibility: ${({ componentVisible }) =>
+		!componentVisible ? 'hidden' : 'visible'};
+	transform: ${({ componentVisible }) =>
+		!componentVisible ? 'scaleY(0)' : 'scaleY(1)'};
+	transition: visibility 200ms, transform 200ms;
+	transform-origin: top;
+`;
+
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
@@ -22,16 +41,56 @@ const Container = styled.div`
 	width: 100%;
 `;
 
-function GenericDropdown({ buttonDisplay, dropdownItems }: Props) {
+export const ListLink = styled(Link)`
+	display: block;
+	height: 100%;
+	text-decoration: none;
+	color: ${({ theme }) => theme.colors.text[2]};
+	padding: 1em;
+	&:hover {
+		color: ${({ theme }) => theme.colors.primary[0]};
+	}
+`;
+
+export const ListButton = styled.div`
+	display: block;
+	height: 100%;
+	text-decoration: none;
+	color: ${({ theme }) => theme.colors.text[2]};
+	padding: 1em;
+	&:hover {
+		color: ${({ theme }) => theme.colors.primary[0]};
+	}
+`;
+
+const HIDE_LIST = -9999;
+
+function GenericDropdown({
+	buttonDisplay,
+	children,
+}: React.PropsWithChildren<Props>) {
 	const ref = useRef<HTMLDivElement>(null);
 	const buttonRef = useRef<HTMLButtonElement>(null);
-	const { isComponentVisible, setIsComponentVisible } = useComponentVisible(
+	const [componentVisible, setComponentVisible] = useComponentVisible(
 		ref,
 		false
 	);
 	const handleClick = () => {
-		setIsComponentVisible(!isComponentVisible);
+		setComponentVisible(!componentVisible);
 	};
+
+	const [listTopPosition, setListTopPosition] = useState(HIDE_LIST);
+
+	useEffect(() => {
+		const getMenuButtonBottom = () => {
+			const menuButton = buttonRef.current;
+			if (menuButton != null) {
+				return menuButton.getBoundingClientRect().bottom;
+			}
+			return HIDE_LIST;
+		};
+		setListTopPosition(getMenuButtonBottom());
+	}, [buttonRef]);
 
 	return (
 		<Container ref={ref}>
@@ -39,17 +98,18 @@ function GenericDropdown({ buttonDisplay, dropdownItems }: Props) {
 				{buttonDisplay}
 				<i
 					className={
-						isComponentVisible
+						componentVisible
 							? 'fas fa-angle-up'
 							: 'fas fa-angle-down'
 					}
 				/>
 			</DropdownButton>
-			<DropdownList
-				buttonRef={buttonRef}
-				items={dropdownItems}
-				isComponentVisible={isComponentVisible}
-			/>
+			<DropdownMenu
+				style={{ top: `${listTopPosition}px` }}
+				componentVisible={componentVisible}
+			>
+				{children}
+			</DropdownMenu>
 		</Container>
 	);
 }

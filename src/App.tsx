e4197f0ee26498ae11b84b5bf0525d7b35b83534
Administrator <admin@192.168.50.74>
2021-09-12 17:06:01 -0700

    initial work

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Pages from 'pages';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
-	return <Pages />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Pages />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
 
 export default App;

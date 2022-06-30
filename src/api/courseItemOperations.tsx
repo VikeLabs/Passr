9fd8c3cdea8be52e82fe8a87326940c5b50bbdc8
isaiahdoyle <71530236+isaiahdoyle@users.noreply.github.com>
2022-06-30 15:58:18 -0700

    PAS-115A: Course item API calls (#88)
    * install react-query
    
    * implement working get request
    
    * basic read/create functions complete(?)
    
    * finalize operations file
    
    * change date type and adjust item id parsing
    
    * move hooks to own file
    
    * handle fetch errors
    
    * remove import in courseItemOperations.tsx
    
    * remove tests/sync with master
    
    * remove unnecessary await & add key to useReadCourseItem
    
    * improve naming and add utils

---
+++ src/api/courseItemOperations.tsx
@@ -0,0 +1,32 @@
+import { CourseItem } from 'api';
+import { AddItemData } from 'components/AddItemModal';
+import { handleErrors } from './utils';
+
+export const createCourseItem = async (data: AddItemData) =>
+	fetch('/courseItem/', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json', userID: 'user1' },
+		body: JSON.stringify(data),
+	}).then(handleErrors);
+
+export const getCourseItem = async (id: string) =>
+	fetch(`/courseItem/${id}`, {
+		headers: { userID: 'user1' }, // REMINDER: REMOVE HADRCODED USER ID
+	})
+		.then(handleErrors)
+		.then((res) => res.json());
+
+export const updateCourseItem = async (change: Partial<CourseItem>) =>
+	fetch('/courseItem/', {
+		method: 'PUT',
+		headers: { 'Content-Type': 'application/json', userID: 'user1' },
+		body: JSON.stringify(change),
+	}).then(handleErrors);
+
+export const deleteCourseItem = async (id: string) =>
+	fetch('/courseItem/', {
+		// append id to end of string
+		method: 'DELETE',
+		headers: { 'Content-Type': 'application/json', userID: 'user1' },
+		body: JSON.stringify({ id }),
+	}).then(handleErrors);
---
+++ src/hooks/useCourseItem.tsx
@@ -0,0 +1,22 @@
+import { useQuery, useMutation } from 'react-query';
+import { CourseItem } from 'api';
+import { AddItemData } from 'components/AddItemModal';
+
+import {
+	getCourseItem,
+	createCourseItem,
+	updateCourseItem,
+	deleteCourseItem,
+} from 'api/courseItemOperations';
+
+export const useReadCourseItem = (id: string) =>
+	useQuery(['courseItem', id], () => getCourseItem(id));
+
+export const useCreateCourseItem = () =>
+	useMutation((data: AddItemData) => createCourseItem(data));
+
+export const useUpdateCourseItem = () =>
+	useMutation((data: Partial<CourseItem>) => updateCourseItem(data));
+
+export const useDeleteCourseItem = () =>
+	useMutation((id: string) => deleteCourseItem(id));
